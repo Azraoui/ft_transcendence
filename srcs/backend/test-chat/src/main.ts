3016570251fdc3c40243711ed4c6192ef57a04fa
Azraoui <ael-azra@student.1337.ma>
2022-12-21 21:36:46 +0100

    abdellah

--- srcs/backend/test-chat/src/main.ts
+++ srcs/backend/test-chat/src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function launch() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
-console.log("Hello World");
+launch();
