f3f40fee20e9ac9a20c85f75bd76561ccfa396b8
Azraoui <ael-azra@student.1337.ma>
2022-12-23 20:00:32 +0100

    abdellah

--- srcs/backend/test-chat/src/app.module.ts
+++ srcs/backend/test-chat/src/app.module.ts
@@ -1,10 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { PrismaService } from './prisma/prisma.service';
 
-@Module({
-  imports: [AuthModule],
-  providers: [PrismaService],
-})
+@Module({})
 
 export class AppModule {}
--- srcs/backend/test-chat/src/prisma/prisma.service.ts
+++ srcs/backend/test-chat/src/prisma/prisma.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Global, INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
+@Global()
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    
+
+    // fun1
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    // fun2
+    async enableShutdownHooks(app: INestApplication) {
+        this.$on('beforeExit', async () => {
+            await app.close();
+        })
+    }
+    
+}
