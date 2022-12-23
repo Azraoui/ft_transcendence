c25a783cc447db6aecfbcf8221d6f0546f62b007
Azraoui <ael-azra@student.1337.ma>
2022-12-23 17:40:36 +0100

    abdellah

--- srcs/backend/test-chat/src/app.module.ts
+++ srcs/backend/test-chat/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [AuthModule],
+  providers: [PrismaService],
 })
 
 export class AppModule {}
