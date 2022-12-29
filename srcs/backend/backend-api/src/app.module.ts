3db66c2208100dd10f6d2353a6064148b2bbc9af
Azraoui <ael-azra@student.1337.ma>
2022-12-29 08:55:32 +0100

    abdellah

--- srcs/backend/backend-api/src/app.module.ts
+++ srcs/backend/backend-api/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
 })
 
 export class AppModule {}
