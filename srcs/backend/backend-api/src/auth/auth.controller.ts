1846c9c27610bc347a5088691ca136244cd13a31
Azraoui <ael-azra@student.1337.ma>
2022-12-30 22:51:20 +0100

    abdellah

--- srcs/backend/backend-api/src/auth/auth.controller.ts
+++ srcs/backend/backend-api/src/auth/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, ExecutionContext, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUserReq } from 'src/decorator';
 import { AuthService } from './auth.service';
 import { FortyTwoOAuthGuard } from './guard/fortytwo-oauth.guard';
 
@@ -16,7 +15,7 @@ export class AuthController {
 
     @UseGuards(FortyTwoOAuthGuard)
     @Get('42-redirect')
-    async fortyTwoAuthRedirect(@Req() req, @GetUserReq() userReq) {
+    async fortyTwoAuthRedirect(@Req() req) {
         const userAndAcessToken =  await this.authService.fortytwoLogin(req.user);
         const request = this.context.switchToHttp().getRequest();
         request.headers['Authorization'] =  `Bearer ${userAndAcessToken.access_token}`;
