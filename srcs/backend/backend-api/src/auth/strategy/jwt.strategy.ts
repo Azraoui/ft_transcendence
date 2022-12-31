dd15bc1853228f0e90ca1149490eb98742399043
Azraoui <ael-azra@student.1337.ma>
2022-12-31 22:20:21 +0100

    abdellah

--- srcs/backend/backend-api/src/auth/strategy/jwt.strategy.ts
+++ srcs/backend/backend-api/src/auth/strategy/jwt.strategy.ts
@@ -1,17 +1,15 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-    });
+      
+    })
   }
 
-  async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
-  }
+
 }
