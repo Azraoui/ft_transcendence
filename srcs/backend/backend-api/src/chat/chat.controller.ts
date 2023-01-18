215c5dc688d522f1d8ebd4960016867b87d1b74f
Azraoui <ael-azra@student.1337.ma>
2023-01-18 16:08:25 +0100

    abdellah

--- srcs/backend/backend-api/src/chat/chat.controller.ts
+++ srcs/backend/backend-api/src/chat/chat.controller.ts
@@ -47,25 +47,33 @@ export class ChatController {
     joinRoom(@Body() body: JoinRoomDto, @GetUserReq('id') userId: number) {
         return this.chatService.joinRoom(body, +userId);
     }
-
+    
     @UseGuards(JwtTwoFactorGuard)
     @Delete('leftRoom/:id')
     leftRoom(@Param('id') roomId: number, @GetUserReq('id') userId: number) {
         return this.chatService.leftRoom(+roomId, +userId);
     }
-
-
+    
     @UseGuards(JwtTwoFactorGuard)
     @Get('viewMembers/:id')
     viewMembers(@Param('id') roomId: number, @GetUserReq('id') userId: number) {
         return this.chatService.viewMembers(+roomId, +userId);
     }
 
-
     @UseGuards(JwtTwoFactorGuard)
+    @HttpCode(HttpStatus.OK)
     @Post('addAdmin')
     addAdmin(@GetUserReq('id') userId: number, @Body() body: any) {
         return this.chatService.addAdmin(body.roomId, userId, body.newAdminId);
     }
+
+    // @UseGuards(JwtTwoFactorGuard)
+    // @HttpCode(HttpStatus.OK)
+    // @Post('muteMember')
+    // muteAdmin(@GetUserReq('id') userId: number, @Body() body: any) {
+    //     return this.chatService.addAdmin(body.roomId, userId, body.newAdminId);
+    // }
+
+
 
 }
--- srcs/backend/backend-api/src/chat/chat.service.ts
+++ srcs/backend/backend-api/src/chat/chat.service.ts
@@ -161,7 +161,7 @@ export class ChatService {
                         admins: {
                             push: newAdminId
                         }
-                    }
+                    },
                 })
                 return room;
             }
